21cbea4a04441324c49971778581628b06e8f0b8
Jonathan.MB <Marinjonathan40@gmail.com>
2023-07-25 14:30:26 -0500

    actualizacion header y footer

---
+++ views/index.php
@@ -0,0 +1,86 @@
+<?php
+require_once('resources/Header1.php')
+?>
+
+<div class="jumbotron jumbotron-fluid">
+    <div class="container d-flex flex-column align-items-center bg-success">
+        <h1 class="display-4">NEW COLECCTIONS</h1>
+        <p class="lead">COLOCAR LA VAINA DE IMAGEN .</p>
+        <div class="image-container">
+            <img src="../img/banner.webp" class="custom-image" alt="Responsive image">
+        </div>
+    </div>
+</div>
+
+
+
+<div class="container text-center bg-primary">
+<h1 class="display-4">Slider</h1> <!-- Contenedor principal centrado -->
+        <div class="row">
+            <!-- Las siguientes clases col-md-3 definen que cada columna ocupe 3/12 (1/4) del ancho total en dispositivos medianos y más grandes -->
+            <div class="col-md-3">
+                <img src="../img/carru2.jpeg" alt="Imagen 1" style="width: 200px; height: 150px;">
+            </div>
+            <div class="col-md-3">
+                <img src="../img/carru3.jpg" alt="Imagen 2" style="width: 200px; height: 150px;">
+            </div>
+            <div class="col-md-3">
+                <img src="../img/carru4.jpg" alt="Imagen 3" style="width: 200px; height: 150px;">
+            </div>
+            <div class="col-md-3">
+                <img src="../img/caru.jpg" alt="Imagen 4" style="width4: 200px; height: 150px;">
+            </div>
+        </div>
+    </div>
+
+
+
+
+<div class="container  bg-warning">
+    <div class="row">
+        <!-- Lado izquierdo -->
+        <div class="col-md-4">
+            <img src="../img/50.jpg" alt="Imagen 1" class="img-fluid img-padding ">
+        </div>
+        <!-- En medio -->
+        <div class="col-md-4 d-flex justify-content-center align-items-center">
+            <button class="btn btn-primary">Botón</button>
+        </div>
+        <!-- Lado derecho -->
+        <div class="col-md-4">
+            <img src="../img/50.jpg" alt="Imagen 2" class="img-fluid img-padding">
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+<div class="container text-center bg-primary">
+<h1 class="display-4">Slider</h1> <!-- Contenedor principal centrado -->
+        <div class="row">
+            <!-- Las siguientes clases col-md-3 definen que cada columna ocupe 3/12 (1/4) del ancho total en dispositivos medianos y más grandes -->
+            <div class="col-md-3">
+                <img src="../img/carru2.jpeg" alt="Imagen 1" style="width: 200px; height: 150px;">
+            </div>
+            <div class="col-md-3">
+                <img src="../img/carru3.jpg" alt="Imagen 2" style="width: 200px; height: 150px;">
+            </div>
+            <div class="col-md-3">
+                <img src="../img/carru4.jpg" alt="Imagen 3" style="width: 200px; height: 150px;">
+            </div>
+            <div class="col-md-3">
+                <img src="../img/caru.jpg" alt="Imagen 4" style="width4: 200px; height: 150px;">
+            </div>
+        </div>
+    </div>
+
+
+
+    
+<?php
+require_once('resources/Footer.php')
+?>
---
+++ views/resources/footer.php
@@ -0,0 +1,62 @@
+<!-- Footer con imagen (LOGO/MEDIOS DE PAGO) -->
+
+<footer class="bg-dark text-center text-lg-start text-white bg-black">
+    <!-- Grid container -->
+    <div class="container p-4 bg-black">
+        <!--Grid row-->
+        <div class="row my-4">
+            <!--Grid column-->
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <div class=" shadow-1-strong d-flex align-items-center justify-content-center mb-4 mx-auto">
+                    <img class="logo" src="../img/BD1.png" alt="" loading="lazy" />
+                </div>
+                <div class="d-flex align-items-center justify-content-center mb-4 mx-auto">
+                    <img class="logo" src="../img/metodos.png">
+                </div>
+            </div>
+            <!-- Footer con imagen (LOGO/MEDIOS DE PAGO) -->
+            <!--COLUMNAS DE Grid column-->
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase mb-4">Atencion al clientte</h5>
+                <h5 class="text-uppercase mb-4">---------------------------</h5>
+                <ul class="list-unstyled">
+                    <li class="mb-2">
+                        <a href="#!" class="text-white"><i class="fas fa-paw pe-3"></i>Contacto</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="#!" class="text-white"><i class="fas fa-paw pe-3"></i>Wsp : 318457645</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="#!" class="text-white"><i class="fas fa-paw pe-3"></i>Servicio al cliente : linea
+                            nacional
+                            0100</a>
+                    </li>
+                    <li class="mb-2">
+                </ul>
+            </div>
+            <!--COLUMNAS DE Grid column-->
+            <!--Grid column-->
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase mb-4">Atencion al clientte</h5>
+                <h5 class="text-uppercase mb-4">---------------------------</h5>
+                <ul class="list-unstyled">
+                    <li class="mb-2">
+                        <a href="#!" class="text-white"><i class="fas fa-paw pe-3"></i>Contacto</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="#!" class="text-white"><i class="fas fa-paw pe-3"></i>Wsp : 318457645</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="#!" class="text-white"><i class="fas fa-paw pe-3"></i>Servicio al cliente : linea
+                            nacional
+                            0100</a>
+                    </li>
+                    <li class="mb-2">
+                </ul>
+            </div>
+            <!--COLUMNAS DE Grid column-->
+        </div>
+    </div>
+</footer>
+</body>
+</html>
