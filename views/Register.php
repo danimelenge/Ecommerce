12ae0661486355f79bb52df89d45bd5ce9a14f8c
Brayan-Echeverry <brayanstiven2004.be@gmail.com>
2023-07-26 18:50:25 -0500

    Registro Usuario
    Update

---
+++ views/Register.php
@@ -0,0 +1,117 @@
+<?php
+require_once('resources/initiator.php')
+?>
+<link rel="stylesheet" href="../css/styleRegister.css" />
+<?php
+require_once('resources/Header1.php')
+?>
+
+<body>
+
+    <div class="d-flex flex-wrap justify-content-center mb-3">
+        <h3>REGISTRO NUEVO USUARIO</h3>
+    </div>
+
+    <div class="con1 container border border-warning p-4">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+
+                <div class="container d-flex flex-column align-items-center">
+
+                    <div class="Name col-8">
+                        <label for="inputName" class="form-label">Nombre</label>
+                        <input type="text" class="form-control" id="inputName">
+                    </div>
+                    <div class="Last_name col-8">
+                        <label for="inputLastName" class="form-label">Apellido</label>
+                        <input type="text" class="form-control" id="inputLastName">
+                    </div>
+
+                    <div class="DateofBirth col-8">
+                        <label for="dateOfBirth" class="form-label">Fecha de Nacimiento</label>
+                        <input type="date" class="form-control" id="dateOfBirth" name="dateOfBirth">
+                    </div>
+
+                    <div class="country col-8">
+                        <label for="inputcountry" class="form-label">Pais</label>
+                        <select class="form-select " aria-label="Default select example" id="inputcountry">
+                            <option selected></option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="2">Three</option>
+
+                        </select>
+                    </div>
+                    <div class="department col-8">
+                        <label for="inputDepartment" class="form-label">Departamento</label>
+                        <select class="form-select  " aria-label="Default select example" id="inputDepartment">
+                            <option selected></option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="2">Three</option>
+
+                        </select>
+                    </div>
+                    <div class="municipality col-8">
+                        <label for="inputMunicipality" class="form-label">Municipio</label>
+                        <select class="form-select " aria-label="Default select example" id="inputMunicipality">
+                            <option selected></option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="2">Three</option>
+
+                        </select>
+                    </div>
+
+                    <div class="address col-8">
+                        <label for="inputAddress" class="form-label">Dirección</label>
+                        <input type="text" class="form-control" id="inputAddress">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="container d-flex flex-column align-items-center">
+
+                    <div class="sex col-8">
+                        <label for="inputSex" class="form-label">Sexo</label>
+                        <input type="text" class="form-control" id="inputSex">
+                    </div>
+                    <div class="Email col-8">
+                        <label for="inputEmail" class="form-label">Correo</label>
+                        <input type="email" class="form-control" id="inputEmail" name="inputEmail">
+                    </div>
+                    <div class="VerEmail col-8">
+                        <label for="inputEmailVerification" class="form-label">Verificar Correo:</label>
+                        <input type="email" class="form-control" id="inputEmailVerification"
+                            name="inputEmailVerification">
+                    </div>
+                    <div class="password col-8">
+                        <label for="inputPassword" class="form-label">Contraseña</label>
+                        <input type="password" class="form-control" id="inputPassword">
+                    </div>
+                    <div class="veriPassword col-8">
+                        <label for="inputPassword2" class="form-label">Verificar Contraseña</label>
+                        <input type="password" class="form-control" id="inputPassword2">
+                    </div>
+
+
+                    <div class="mb-3 form-check" style="padding: 20px;">
+                        <input type="checkbox" class="form-check-input" id="terminos" name="terminos">
+                        <label class="form-check-label" for="terminos">
+                            Aceptar <a href="">términos y condiciones</a>
+                        </label>
+                    </div>
+                    <div class="container d-flex flex-column align-items-center" style="padding: 10px;">
+
+                        <button type="button" class="btn">Iniciar Sesión</button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ views/editUser.php
@@ -0,0 +1,117 @@
+<?php
+require_once('resources/initiator.php')
+?>
+<link rel="stylesheet" href="../css/styleEditUser.css">
+<?php
+require_once('resources/header2.php')
+?>
+
+<body>
+  -<div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">espacio para Header</header>
+    <div class="d-flex flex-wrap justify-content-center">
+      <h1>EDITAR PERFIL</h1>
+    </div>
+
+
+  </div>
+  <form class=" sc container border border-warning  p-4 ">
+    <div class="row">
+      <div class="col-md-6">
+
+        <div class="container d-flex flex-column align-items-center">
+          <!-- Contenido del primer div container -->
+          <div class="logo col-8" id="EditImage">
+            <img src="../img/logo_pato.png" alt="">
+            <!-- <h1>aqui va img</h1> -->
+          </div>
+          <br>
+
+          <div class="Edit_name col-8">
+            <label for="EditName" class="form-label">Editar nombre</label>
+            <input type="text" class="form-control " id="EditName" aria-describedby="emailHelp">
+          </div>
+
+          <div class="Edit_lastname col-8">
+            <label for="EditLastName" class="form-label">Editar apellidos</label>
+            <input type="text" class="form-control" id="EditLastName" aria-describedby="emailHelp">
+          </div>
+
+          <div class="sex col-8">
+            <label for="EditSex" class="form-label">Sexo</label>
+            <input type="text" class="form-control " id="EditSex" aria-describedby="emailHelp">
+          </div>
+
+          <div class="birth_date col-8">
+            <label for="EditDateOfBirth" class="form-label">Fecha Nacimiento</label>
+            <input type="date" class="form-control" id="EditDateOfBirth" name="dateOfBirth">
+          </div>
+
+        </div>
+      </div>
+
+      <div class="col-md-6">
+        <div class="container d-flex flex-column align-items-center">
+          <!-- <div class="" style="background-color: #ffffff; padding: 20px;"> -->
+          <!-- Contenido del segundo div container -->
+          <div class="country col-8">
+            <label for="EditCountry" class="form-label">Pais</label>
+            <select class="form-select " id="EditCountry" aria-label="Default select example">
+              <option selected></option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+
+            </select>
+          </div>
+          <div class="department col-8">
+            <label for="EditDepartament" class="form-label">Departamento</label>
+            <select class="form-select  " id="EditDepartament" aria-label="Default select example">
+              <option selected></option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+
+            </select>
+          </div>
+          <div class="municipality col-8">
+            <label for="EditMunicipality" class="form-label">Municipio</label>
+            <select class="form-select " id="EditMunicipality" aria-label="Default select example">
+              <option selected></option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+
+            </select>
+            <div class="address col-14">
+              <label for="EditAddress" class="form-label">Direccion</label>
+              <input type="text" class="form-control" id="EditAddress" aria-describedby="emailHelp">
+            </div>
+            <div class="current_pass col-14">
+              <label for="CurrentPass" class="form-label">Contraseña actual</label>
+              <input type="password" class="form-control" id="CurrentPass" aria-describedby="emailHelp">
+            </div>
+            <div class="new_pass col-14">
+              <label for="NewPass" class="form-label">Contraseña nueva</label>
+              <input type="password" class="form-control" id="NewPass" aria-describedby="emailHelp">
+            </div>
+            <div class="verify_pass col-14">
+              <label for="VerifyPass" class="form-label">Verificar contraseña</label>
+              <input type="password" class="form-control" id="VerifyPass" aria-describedby="emailHelp">
+              <br>
+            </div>
+            <!-- <div  id="btn" class="container d-flex flex-column align-items-center">
+                                    <button type="button" class="btn  ">Guardar</button>
+                                </div> -->
+            <div class="container d-flex flex-column align-items-center" style="padding: 10px;">
+
+              <button type="button" class="btn ">Iniciar Sesión</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </form>
+  
+
+
+</body>
+
+</html>
